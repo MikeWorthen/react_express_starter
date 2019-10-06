84931c45f4768022943c585c85a1273744a34e1d
mworth <mikeworthen55@gmail.com>
2019-10-06 12:08:14 -0600

    book search is working and connected to api

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import AppNavbar from './components/AppNavbar';
+import Search from "./pages/Search";
+import Results from "./pages/Results";
+import Saved from "./pages/Saved";
+import NoMatch from "./pages/NoMatch";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-class App extends Component {
-  render() {
-  return (
-    <div className="App">
-      <AppNavbar />
+const App = () => (
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path="/" component={Search} />
+        <Route exact path="/results" component={Results} />
+        <Route exact path="/saved" component={Saved} />
+        <Route component={NoMatch} />
+      </Switch>
     </div>
-  );
-}
-
-}
+  </Router>
+);
 
 export default App;
