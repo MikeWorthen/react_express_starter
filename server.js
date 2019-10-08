2f97499cdb005270f86bfe9cda4810a2d29abade
Mike Worthen <mikeworthen55@gmail.com>
2019-10-07 18:48:48 -0600

    fixed port

--- server.js
+++ server.js
@@ -22,4 +22,4 @@ app.use('/api/books', books)
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, () => console.log( "Server started on port" + port));
